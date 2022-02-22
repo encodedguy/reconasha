4661e4538695082914eb508b64cb80d9306a5ffa
3nc0d3dGuY <singhinder1208@gmail.com>
2022-02-22 09:11:59 +0530

    Added more

--- reconasha.sh
+++ reconasha.sh
@@ -1,7 +1,11 @@
 amass enum -noalts -nolocaldb -df domains.txt -min-for-recursive 7 -passive -o subdomains-amass-passive.txt & subfinder -dL domains.txt -silent -nC -nW -o subdomains-subfinder.txt -t 100
 cat domains.txt | assetfinder --subs-only | tee subdomains-assetfinder.txt
+cat domains.txt | parallel -j 10 ~/go/bin/crobat -s {} 2> /dev/null | tee subdomains-crobat.txt
+cat domains.txt | ~/go/bin/haktrails subdomains | tee subdomains-haktrails.txt
+cat domains.txt | parallel -j 10 shosubgo -d {} -s $SHODANAPIKEY 2> /dev/null | tee subdomains-shosubgo.txt
 cat subdomains-* | sort -u | tee subdomains.txt
 cat subdomains.txt | httpx -sc -server -title -ip -cname -silent -threads 100 -o httpx.txt 
+subjack -w subdomains.txt -t 300 -o subjack.txt0
 cat httpx.txt | cut -d' ' -f1 | tee live.txt
 mkdir nuclei; nuclei -silent -l live.txt -t cves -o nuclei/cves.txt -rl 300
 nuclei -l live.txt -silent -t exposures -rl 300 -o nuclei/exposures.txt
