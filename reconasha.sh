f4688897c14f187c7f6f0be2355f6742292907a1
3nc0d3dGuY <singhinder1208@gmail.com>
2022-02-24 04:39:15 +0530

    Little more update

--- reconasha.sh
+++ reconasha.sh
@@ -4,10 +4,10 @@ cat domains.txt | parallel -j 10 ~/go/bin/crobat -s {} 2> /dev/null | tee subdom
 cat domains.txt | ~/go/bin/haktrails subdomains | tee subdomains-haktrails.txt
 cat domains.txt | parallel -j 10 shosubgo -d {} -s $SHODANAPIKEY 2> /dev/null | tee subdomains-shosubgo.txt
 cat subdomains-* | sort -u | tee subdomains.txt
-cat subdomains.txt | httpx -sc -server -title -ip -cname -silent -threads 100 -o httpx.txt 
-subjack -w subdomains.txt -t 300 -o subjack.txt0
+cat subdomains.txt | httpx -sc -server -title -ip -cname -silent -threads 100 -o httpx.txt & subjack -w subdomains.txt -t 300 -o subjack.txt
 cat httpx.txt | cut -d' ' -f1 | tee live.txt
 mkdir nuclei; nuclei -silent -l live.txt -t cves -c 50 -o nuclei/cves.txt -rl 300
 nuclei -l live.txt -silent -t exposures -t exposed-panels -rl 300 -c 50 -o nuclei/exposures_exposed-panels.txt
 nuclei -l live.txt -silent -t misconfiguration -rl 300 -c 50 -o nuclei/misconfiguration.txt
 nuclei -l live.txt -silent -t takeovers -t default-logins -rl 300 -c 50 -o nuclei/takeovers_default-logins.txt
+echo subdomains.txt | gau | tee gau-output.txt
