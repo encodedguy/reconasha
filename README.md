0f4a5acaf4967c59fc2e8ea6979546ffcbdce37a
3nc0d3dGuY <singhinder1208@gmail.com>
2022-06-22 15:07:33 -0400

    Updated script and readme

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
 ## reconasha
-Let the reconasha do the automation with all the powerful bugbounty tools.
+Let the reconasha do the automation for you.
 
 ## Installation
 ### Kali-Linux
 Check /etc/apt/sources.list file for installation sources first.
 
-``sudo apt-get update
-sudo apt-get install golang python3 -y
-sudo apt-get install amass httpx nuclei subfinder assetfinder -y``
-
+``sudo apt-get update``
+``sudo apt-get install golang python3 -y``
+``sudo apt-get install amass httpx nuclei subfinder assetfinder -y``
+``Install subjack, gau from official repository``
 ``export SHODANAPIKEY=<YOUR-API-KEY>``
+``git clone https://github.com/encodedguy/reconasha``
+``cd reconasha``
 
 ## Usage
-Add domains to domains.txt
-
-Make sure domains.txt is at same directory
-
-``chmod +x reconasha.sh
-.reconasha.sh``
+``chmod +x reconasha.sh``
+``./reconasha.sh domains.txt``
--- reconasha.sh
+++ reconasha.sh
@@ -1,4 +1,5 @@
 echo "[-] Starting reconasha.sh"
+echo "Author: @encodedguy"
 echo "[-]" $1
 echo ""
 sleep 2
@@ -9,8 +10,6 @@ subfinder -dL $1 -silent -nC -nW -o subdomains-subfinder.txt -t 100 > /dev/null 
 echo "[+] Starting Subfinder" &
 cat $1 | assetfinder --subs-only | tee subdomains-assetfinder.txt > /dev/null &
 echo "[+] Starting Assetfinder" &
-cat $1 | parallel -j 10 ~/go/bin/crobat -s {} 2> /dev/null | tee subdomains-crobat.txt > /dev/null &
-echo "[+] Starting Crobat" &
 cat $1 | ~/go/bin/haktrails subdomains | tee subdomains-haktrails.txt > /dev/null &
 echo "[+] Starting Haktrails" &
 cat $1 | parallel -j 10 shosubgo -d {} -s $SHODANAPIKEY 2> /dev/null | tee subdomains-shosubgo.txt > /dev/null &
@@ -29,9 +28,9 @@ echo""
 cat subdomains-* | sort -u | tee subdomains.txt > /dev/null;
 subdomains=`wc -l subdomains.txt`;
 
-cat subdomains.txt | httpx -sc -server -title -ip -cname -silent -threads 1000 -o httpx.txt > /dev/null &
+cat subdomains.txt | httpx -sc -server -title -ip -cname -silent -t 300 -o httpx.txt > /dev/null &
 echo "[+] Starting Httpx" &
-subjack -w subdomains.txt -t 500 -o subjack.txt > /dev/null &
+subjack -w subdomains.txt -t 100 -ssl -o subjack.txt > /dev/null &
 echo "[+] Starting Subjack" &
 wait;
 
@@ -44,13 +43,14 @@ cat httpx.txt | cut -d' ' -f1 | tee live.txt > /dev/null;
 live=`wc -l live.txt`;
 subjack=`wc -l subjack.txt`;
 
-mkdir nuclei; nuclei -silent -l live.txt -t cves -c 50 -o nuclei/cves.txt -rl 1500 -c 100 > /dev/null &
+mkdir nuclei;
+nuclei -silent -l live.txt -t cves -c 50 -o nuclei/cves.txt -rl 500 > /dev/null &
 echo "Starting Nuclei-Engine" &
-nuclei -l live.txt -silent -t exposures -t exposed-panels -rl 1500 -c 100 -o nuclei/exposures_exposed-panels.txt > /dev/null &
+nuclei -l live.txt -silent -t exposures -t exposed-panels -rl 500 -c 50 -o nuclei/exposures_exposed-panels.txt > /dev/null &
 wait;
 
-nuclei -l live.txt -silent -t misconfiguration -rl 1500 -c 50 -o nuclei/misconfiguration.txt > /dev/null &
-nuclei -l live.txt -silent -t takeovers -t default-logins -rl 1500 -c 100 -o nuclei/takeovers_default-logins.txt > /dev/null &
+nuclei -l live.txt -silent -t misconfiguration -rl 500 -c 50 -o nuclei/misconfiguration.txt > /dev/null &
+nuclei -l live.txt -silent -t takeovers -t default-logins -rl 500 -c 50 -o nuclei/takeovers_default-logins.txt > /dev/null &
 wait;
 
 echo "[=] Finished Nuclei-Engine"
